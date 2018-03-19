1efdf0b9e980d6aa7d2e8ee522031767283dd741
mathewcmartin <chactasstryker@gmail.com>
2018-03-18 21:06:09 -0700

    string1.py def donuts(count): excercise

--- GooglePythonClass/google-python-exercises/basic/string1.py
+++ GooglePythonClass/google-python-exercises/basic/string1.py
@@ -24,8 +24,12 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  count = numOfDonuts
+  if numOfDonuts >= 10:
+      return '"Number of donuts: " + count'
+  else:
+      return 'Number of donuts: many'
+
 
 
 # B. both_ends
@@ -91,7 +95,7 @@ def main():
   test(both_ends('a'), '')
   test(both_ends('xyz'), 'xyyz')
 
-  
+
   print
   print 'fix_start'
   test(fix_start('babble'), 'ba**le')
